1a2c13383f130f2ca2b2b283db3c304e1651274b
Dylan Smith <dylansmith86@gmail.com>
2020-03-04 15:16:10 -0700

    using state with functional components. Added button to update age

--- first_reactts/src/components/PersonCard.tsx
+++ first_reactts/src/components/PersonCard.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from "react";
+
 interface Person {
     firstName: string;
     lastName: string;
@@ -7,11 +8,21 @@ interface Person {
 }
 
 const PersonCard: React.FC <Person> = props => {
+    const [state, setState] = useState({
+        ageCount: props.age
+    });
+    const clickHandler = () => {
+        console.log(state);
+        setState({
+            ageCount: state.ageCount + 1
+        });
+    };
     return (
         <div className="personCard">
             <h1 className="lead">{props.lastName}, {props.firstName}</h1>
-            <p>Age: {props.age}</p>
+            <p>Age: {state.ageCount}</p>
             <p>Hair Color: {props.hairColor}</p>
+            <button onClick = {clickHandler}>Happy Birthday {props.firstName} {props.lastName}</button>
         </div>
     );
 }

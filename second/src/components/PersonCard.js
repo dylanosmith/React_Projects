7f047098a5eafe34a5ae9fcf811bd3bcab67977e
Dylan Smith <dylansmith86@gmail.com>
2020-03-04 12:01:59 -0700

    learning react with typescript.

--- second/src/components/PersonCard.js
+++ second/src/components/PersonCard.js
@@ -4,11 +4,6 @@ import React, { Component } from 'react';
 class PersonCard extends Component {
     constructor(props){
         super(props);
-        const {firstName, lastName, age, hairColor } = this.props;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.hairColor = hairColor;
         this.state = {
             ageCount: this.age
         }
@@ -19,12 +14,13 @@ class PersonCard extends Component {
         })
     }
     render() {
+        const {firstName, lastName, age, hairColor } = this.props;
         return(
             <div>
-                <h1>{this.lastName}, {this.firstName}</h1>
+                <h1>{lastName}, {firstName}</h1>
                 <h6>Age:{this.state.ageCount}</h6>
-                <h6>Hair Color: {this.hairColor}</h6>
-                <button onClick = {this.handleClick}>Birthday Button for {this.firstName} {this.lastName}</button>
+                <h6>Hair Color: {hairColor}</h6>
+                <button onClick = {this.handleClick}>Birthday Button for {firstName} {lastName}</button>
             </div>
         );
     }

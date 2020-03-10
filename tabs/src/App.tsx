124995b328d4b64c7a43bf8d063d4b3c617be32f
Dylan Smith <dylansmith86@gmail.com>
2020-03-10 11:03:44 -0600

    updated Tabs assignment. Still need to add logic for changing background color on selection

--- tabs/src/App.tsx
+++ tabs/src/App.tsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Tab from './components/Tab';
 
+interface ButtonProps {
+  textKey:() => void,
+  label:string
+  isSelected?: boolean
+}
+
+const Button:React.FC<ButtonProps> = ({textKey, label, isSelected}) => {
+  return (
+    <button onClick= {textKey} className = {isSelected?'selected': 'notSelected'}>Tab {label}</button>)
+}
+
+interface DisplayProps {
+  message: string;
+}
+
+const Display:React.FC<DisplayProps> = ({message}) => {
+  return (
+    <div className = 'display'>
+      {message}
+    </div>
+  );
+}
 
 function App() {
+  const [textNumber, setTextNumber ] = useState<number>(0);
+  const changeLogger = (incValue:number) => {
+    setTextNumber(incValue)
+  }
+
+  const textOptions = [
+    'Text for Tab A',
+    'Text for Tab B',
+    'Text for Tab C',
+    'Text for Tab D',
+  ]
   return (
     <div className="App">
-      <div id="tab_bar">
-        <Tab 
-          index={1} label = {"Tab1"} content = {"In a while crocodile"} isSelected ={true}>
-        </Tab>
-        <Tab 
-          index ={2} label = {"Tab2"} content = {"Take a break bro!"} isSelected = {false}>
-        </Tab>
-        <Tab index ={3} label = {"Tab3"} content = {"So freaking cool!"} isSelected = {false}/>
-      </div>
+      <Button textKey={() => changeLogger(0)} label ="A" isSelected = {true} />
+      <Button textKey={() => changeLogger(1)} label ="B" />
+      <Button textKey={() => changeLogger(2)} label ="C" />
+      <Button textKey={() => changeLogger(3)} label ="D" />
+      <Display message = {textOptions[textNumber]} />
     </div>
   );
 }

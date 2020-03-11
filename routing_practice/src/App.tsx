5509b5faefecbe55b3b0e5fa5f7dbb774dbab2d6
Dylan Smith <dylansmith86@gmail.com>
2020-03-11 14:22:49 -0600

    Initial commit

---
+++ routing_practice/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import { Navbar } from './components/Navbar';
+import {Router} from '@reach/router';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Router>
+
+
+
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ routing_practice/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from '@reach/router';
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <h1>Welcome to my routing site</h1>
+            <ul>
+                <li> <Link to = "/home">Home</Link></li>
+                <li> <Link to = "/4">Number 4</Link></li>
+                <li> <Link to = "/hello">Hello</Link></li>
+                <li> <Link to = "/hello/blue/red">Blue or Red</Link></li>
+            </ul>
+        </nav>
+    )
+}
